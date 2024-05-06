749cea37c137e21913890d30dc10f0a53e5106a7
Aryaman006 <151651851+Aryaman006@users.noreply.github.com>
2024-05-06 09:04:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 connectDB();
 
 const corsOption = {
-        origin:"http://localhost:5173",
+        origin:"https://chat-app-silk-psi.vercel.app/",
         methods:"GET,POST,PUT,DELETE,PATCH,HEAD",
         credentials:true
 }
